fb5d2f3732e0121006a134512ee71b5ab94de9ed
Paloma Tejeda <palomatejeda81@gmail.com>
2025-03-11 17:26:30 +0100

    update footer styling and content

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -11,6 +11,12 @@
           Ébéniste passionné depuis 1995, je conçois et réalise tous types de
           meubles sur-mesure, en alliant tradition artisanale et modernité.
         </p>
+        <div class="footer__text__column__row-links">
+          <NuxtLink to="/stage-ebenisterie">Découvrir l’ébénisterie</NuxtLink
+          ><NuxtLink to="/outil-explorateur-de-materiaux"
+            >Explorateur de matériaux</NuxtLink
+          >
+        </div>
       </div>
       <ul class="footer__text__column">
         <li class="footer__text__column__link">
@@ -44,6 +50,9 @@
           <NuxtLink to="/contact#faq" class="nuxt-link">F.A.Q.</NuxtLink>
         </li>
         <li class="footer__text__column__link">
+          <NuxtLink to="/" class="nuxt-link">Accueil</NuxtLink>
+        </li>
+        <li class="footer__text__column__link">
           <NuxtLink to="/contact" class="nuxt-link">Contact</NuxtLink>
         </li>
         <li class="footer__text__column__link">
@@ -74,7 +83,7 @@
 
   &__text {
     display: flex;
-    gap: 2rem;
+    gap: 4rem;
     width: 100%;
     justify-content: space-between;
     flex-wrap: wrap;
@@ -87,6 +96,19 @@
       max-width: 496px;
       list-style: none;
       color: $base-color;
+
+      &__row-links {
+        display: flex;
+        justify-content: space-between;
+        gap: 1rem;
+        align-items: center;
+        flex-wrap: wrap;
+
+        a {
+          color: $base-color;
+          white-space: nowrap;
+        }
+      }
 
       &__link {
         color: $base-color;
@@ -102,10 +124,14 @@
     display: flex;
     gap: 1rem;
     color: $secondary-color;
+    flex-wrap: wrap;
 
     a {
       color: $secondary-color;
-      margin-left: auto;
+      white-space: nowrap;
+      @media (min-width: $big-tablet-screen) {
+        margin-left: auto;
+      }
     }
   }
 }

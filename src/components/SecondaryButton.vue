11507000613b369ae3c65aa1c21d822418185fbc
Paloma Tejeda <palomatejeda81@gmail.com>
2025-03-31 22:15:03 +0200

    add references to individual furniture pages

---
+++ src/components/SecondaryButton.vue
@@ -0,0 +1,94 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { colors } from "@/utils/colors";
+
+interface Props {
+  variant?:
+    | "chocolate-martini"
+    | "mocha-mousse"
+    | "cannoli-cream"
+    | "nebulosity"
+    | "lucent-white";
+  direction?: "row" | "row-reverse" | "column" | "column-reverse";
+  icon?: string;
+  iconSize?: string;
+  fontSize?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  variant: "chocolate-martini",
+  direction: "row",
+  fontSize: "1rem",
+  radius: "",
+});
+
+const iconColor = computed(() => {
+  switch (props.variant) {
+    case "mocha-mousse":
+      return colors["lucent-white"];
+    case "cannoli-cream":
+      return colors["chocolate-martini"];
+    case "nebulosity":
+      return colors["lucent-white"];
+    case "lucent-white":
+      return colors["nebulosity"];
+    default:
+      return colors["cannoli-cream"];
+  }
+});
+</script>
+<template>
+  <button
+    type="button"
+    class="button noselect"
+    :class="variant"
+    :style="{ flexDirection: direction, fontSize }"
+  >
+    <IconComponent
+      v-if="icon"
+      :icon
+      :size="iconSize || undefined"
+      :color="iconColor"
+    />
+    <slot />
+  </button>
+</template>
+<style scoped lang="scss">
+.button {
+  width: 100%;
+  max-width: 375px;
+  padding: 0.75rem 1rem;
+  cursor: pointer;
+  white-space: nowrap;
+}
+
+.chocolate-martini {
+  background-color: transparent;
+  color: $secondary-color;
+  border: 2px solid $secondary-color;
+}
+
+.mocha-mousse {
+  background-color: $tertiary-color;
+  color: $primary-color;
+  border: 2px solid $tertiary-color;
+}
+
+.cannoli-cream {
+  background-color: $base-color;
+  color: $secondary-color;
+  border: 2px solid $base-color;
+}
+
+.nebulosity {
+  background-color: $text-color;
+  color: $primary-color;
+  border: 2px solid $text-color;
+}
+
+.lucent-white {
+  background-color: $primary-color;
+  color: $text-color;
+  border: 2px solid $primary-color;
+}
+</style>
--- src/pages/bureaux-et-comptoirs-sur-mesure/[slug].vue
+++ src/pages/bureaux-et-comptoirs-sur-mesure/[slug].vue
@@ -36,71 +36,173 @@ const breadcrumbs = [
 </script>
 <template>
   <section class="furniture-page">
-    <h1 class="furniture-page__title">{{ furniture.title }}</h1>
-    <h2 class="furniture-page__subtitle">{{ furniture.subtitle }}</h2>
-    <div
-      class="furniture-page__richtext"
-      v-html="renderRichText(furniture.description)"
-    ></div>
-    <NuxtLink
-      v-if="furniture.collaborationText && furniture.collaborationLink"
-      class="furniture-page__collaboration"
-      :to="furniture.collaborationLink"
-      ><IconComponent icon="handshake" size="2rem" />{{
-        furniture.collaborationText
-      }}</NuxtLink
-    >
-    <NuxtLink
-      to="/contact-ebeniste-savoie"
-      style="margin-top: auto"
-      aria-label="Parlons de votre projet"
-    >
-      <PrimaryButton>Parlons de votre projet</PrimaryButton></NuxtLink
-    >
+    <div class="furniture-page__wrapper">
+      <div class="furniture-page__wrapper__txt">
+        <h1 class="furniture-page__wrapper__txt__title">
+          {{ furniture.title }}
+        </h1>
+        <h2 class="furniture-page__wrapper__txt__subtitle">
+          {{ furniture.subtitle }}
+        </h2>
+        <div
+          class="furniture-page__wrapper__txt__richtext"
+          v-html="renderRichText(furniture.description)"
+        ></div>
+        <NuxtLink
+          v-if="furniture.collaborationText && furniture.collaborationLink"
+          class="furniture-page__wrapper__txt__collaboration"
+          :to="furniture.collaborationLink"
+          ><IconComponent icon="handshake" size="2rem" />{{
+            furniture.collaborationText
+          }}</NuxtLink
+        >
+
+        <NuxtLink
+          to="/contact-ebeniste-savoie"
+          style="margin-top: auto"
+          aria-label="Parlons de votre projet"
+        >
+          <PrimaryButton>Parlons de votre projet</PrimaryButton></NuxtLink
+        >
+      </div>
+      <ImageSlider :images="furniture.images" />
+    </div>
+
+    <div class="furniture-page__illustrations">
+      <div
+        class="furniture-page__illustrations__references"
+        v-if="furniture.references.length > 0"
+      >
+        <div
+          class="furniture-page__illustrations__references__reference"
+          v-for="reference in furniture.references"
+        >
+          <img
+            class="furniture-page__illustrations__references__reference__img"
+            :src="reference.image.filename"
+            :alt="reference.name"
+          />
+          <div
+            class="furniture-page__illustrations__references__reference__txt"
+          >
+            <span>{{ reference.name }}</span
+            ><span>{{ reference.reference }}</span>
+          </div>
+        </div>
+        <SecondaryButton
+          ><NuxtLink to="/materiaux"
+            >Voir d'autres références</NuxtLink
+          ></SecondaryButton
+        >
+      </div>
+    </div>
   </section>
   <JsonldBreadcrumb :links="breadcrumbs" />
 </template>
 <style lang="scss" scoped>
 .furniture-page {
   display: flex;
-  flex-direction: column;
   gap: 2rem;
   padding: 2rem 1rem;
+  flex-direction: column;
 
   @media (min-width: $big-tablet-screen) {
-    padding: 4rem;
+    padding: 2rem 4rem;
+    gap: 4rem;
   }
 
-  &__title {
-    font-size: $medium-title-size;
-    font-weight: $bold;
-  }
+  &__wrapper {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    gap: 2rem;
 
-  &__subtitle {
-    font-size: $medium-text-size;
-    font-weight: $bold;
-  }
+    @media (min-width: $big-tablet-screen) {
+      flex-direction: row;
+    }
 
-  &__richtext {
-    &:deep(ul) {
+    &__txt {
       display: flex;
       flex-direction: column;
       gap: 2rem;
-      list-style: none;
-      height: 100%;
+      width: 100%;
+      max-height: 555px;
 
-      @media (min-width: $big-tablet-screen) {
-        min-width: 320px;
-        max-width: 320px;
-        width: 100%;
+      &__title {
+        font-size: $medium-title-size;
+        font-weight: $bold;
+      }
+
+      &__subtitle {
+        font-size: $medium-text-size;
+        font-weight: $bold;
+      }
+
+      &__richtext {
+        &:deep(ul) {
+          display: flex;
+          flex-direction: column;
+          gap: 2rem;
+          list-style: none;
+          height: 100%;
+
+          @media (min-width: $big-tablet-screen) {
+            min-width: 320px;
+            max-width: 320px;
+            width: 100%;
+          }
+        }
+      }
+
+      &__collaboration {
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
       }
     }
   }
 
-  &__collaboration {
+  &__illustrations {
     display: flex;
-    align-items: center;
-    gap: 0.5rem;
+    flex-direction: column;
+    gap: 2rem;
+    width: 100%;
+
+    &__references {
+      display: grid;
+      grid-template-columns: repeat(auto-fill, minmax(288px, 1fr));
+      width: 100%;
+      gap: 1rem;
+
+      &__reference {
+        display: flex;
+        align-items: center;
+        font-size: $main-text-size;
+        font-weight: $regular;
+        background-color: $base-color-darker;
+        padding: 1rem;
+        gap: 1rem;
+        border-radius: $radius;
+
+        &__img {
+          width: 60px;
+          height: 60px;
+          object-fit: cover;
+          object-position: center;
+          border-radius: calc($radius / 2);
+        }
+
+        &__txt {
+          display: flex;
+          flex-direction: column;
+          gap: 0.5rem;
+
+          & span {
+            white-space: nowrap;
+          }
+        }
+      }
+    }
   }
 }
 </style>
